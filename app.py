9bdd2f384484b36e28969d81a1168495bae26cf6
diawando <abduldiawade777@gmail.com>
2023-02-09 18:23:16 +0000

    correction de la premiere ligne dans schema sql

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import sqlite3
 from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
 def get_db_connection():
     conn= sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-app = Flask(__name__)
+
 
 @app.route('/')
 def index():
