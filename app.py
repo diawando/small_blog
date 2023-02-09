f048643d331709244c954355a1598d3d52a90746
diawando <abduldiawade777@gmail.com>
2023-02-09 18:49:04 +0000

    creation de la fonction get_post(post_id)

--- app.py
+++ app.py
@@ -10,7 +10,14 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 @app.route('/')
 def index():
